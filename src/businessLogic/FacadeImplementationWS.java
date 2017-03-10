8ea7a6fb8cdccc6df50683992781800770091c54
ehu <ehu@158.227.138.43>
2017-03-10 11:52:14 +0100

    prueba1

--- src/businessLogic/FacadeImplementationWS.java
+++ src/businessLogic/FacadeImplementationWS.java
@@ -24,7 +24,7 @@ import exceptions.WrongPassword;
 @WebService(endpointInterface = "businessLogic.ApplicationFacadeInterfaceWS")
 public class FacadeImplementationWS implements ApplicationFacadeInterfaceWS {
 
-	
+	int ernacional;
 	private int nextHouseNumber;
 	/**
 	 * 
